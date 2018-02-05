968daa4c74b60f099e4172601a489c998177cac0
John Castillo <JohnluisCastillo@gmail.com>
2018-02-05 11:59:54 -0500

    fields

--- src/components/ReduxForm.js
+++ src/components/ReduxForm.js
@@ -3,30 +3,29 @@ import {reduxForm, Field} from 'redux-form';
 import {required,nonEmpty,exactLength,onlyNumbers} from '../validate';
 
 
-export default class Form extends React.Component {
+export class ReduxForm extends React.Component {
     render() {
         return (
             <div class='delivery-form'>
                 <h2>Report a problem with your delivery</h2>
-                <form>
+                <form onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
                     <div class="form-input">
                         <label for="trackingNumber">Tracking number</label>
-                        <input name="trackingNumber" value="" id="trackingNumber">
-                        </input>
+                        <Field name="trackingNumber" value="" id="trackingNumber" component='input'/>
                     </div>
                     <div class="form-input">
                         <label for="issue">What is your issue?</label>
-                        <select name="issue" id="issue">
+                        <Field name="issue" id="issue" component='select'>
                             <option value="not-delivered">My delivery hasn't arrived</option>
                             <option value="wrong-item">The wrong item was delivered</option>
                             <option value="missing-part">Part of my order was missing</option>
                             <option value="damaged">Some of my order arrived damaged</option>
                             <option value="other">Other (give details below)</option>
-                        </select>
+                        </Field>
                     </div>
                     <div class="form-input">
                         <label for="details">Give more details (optional)</label>
-                        <textarea name="details" id="details"></textarea>
+                        <Field name="details" id="details" component='textarea'/>
                     </div>
                     <button type="submit">Submit</button>
                 </form>
@@ -34,3 +33,8 @@ export default class Form extends React.Component {
         );
     }
 }
+
+export default reduxForm({ 
+    form: 'ReduxForm',
+
+})(ReduxForm)
